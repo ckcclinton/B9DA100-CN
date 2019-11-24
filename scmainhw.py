20832b237b5a27c313632acb290ad6e75f836eb8
Clinton Ngan <clinton_ngan@hotmail.com>
2019-11-24 14:33:47 +0000

    spellcheckermain

---
+++ scmainhw.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 23 14:48:23 2019
+
+@author: clintonngan
+"""
+
+
+import os
+from spellchecker import SpellChecker
+
+def dir_files(directory):
+    file_names = []
+    for file in os.listdir(directory):
+        if file.endswith(".txt"):
+            file_names.append(file)
+    
+    return file_names
+
+def load_dir_files(file_names):
+    lines_dir = open(file_names).readlines()
+    return list(map(lambda x: x.strip().lower(), lines_dir))
+
+file_names = dir_files("C:/Users/clintonngan/.spyder-py3")
+
+contents = load_dir_files(file_names[2])
+for index, line in enumerate(contents):
+    print(f"Line {index}: {line}")
+    print()
+    
+#print(load_dir_files(file_names[0]), "\n")
+    
+sc = SpellChecker()
+sc.load_words('spell.words')
+for index, line in enumerate(contents):
+    word_list = sc.check_words(line, index)
+    if(len(word_list) != 0):
+        print(word_list)
+
