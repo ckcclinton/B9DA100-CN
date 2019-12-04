c8f2a4b4f45d0d053e8318857bbf04d1a2de9cbf
Clinton Ngan <clinton_ngan@hotmail.com>
2019-12-04 20:50:26 +0000

    bill_management_test

--- bill_management_test.py
+++ bill_management_test.py
@@ -6,15 +6,23 @@ Created on Wed Dec  4 19:25:38 2019
 """
 import unittest
 
-from bill_management import read_bills
+from bill_management import read_bills, write_bills
 
 class TestBillManagement(unittest.TestCase):
     
-    def test_bill_management(self):
+    def test_read_bills(self):
         bills = read_bills()
         self.assertEqual(20, len(bills))
         self.assertEqual('Electric Ireland', bills[0][0])
-        self.assertEqual(' credit', bills[19][6])
+        self.assertEqual('credit', bills[19][6])
+        
+    def test_write_bills(self):
+        bills = read_bills()
+        write_bills(bills)
+        bills = read_bills()
+        self.assertEqual(20, len(bills))
+        self.assertEqual('Electric Ireland', bills[0][0])
+        self.assertEqual('credit', bills[19][6])
         
 if __name__ == '__main__':
     unittest.main()
