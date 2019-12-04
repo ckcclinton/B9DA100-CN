49dfe82bc64845301c6999c236da803d7ffa829d
Clinton Ngan <clinton_ngan@hotmail.com>
2019-12-04 20:01:14 +0000

    bill_management_test

---
+++ bill_management_test.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  4 19:25:38 2019
+
+@author: clintonngan
+"""
+import unittest
+
+from bill_management import read_bills
+
+class TestBillManagement(unittest.TestCase):
+    
+    def test_bill_management(self):
+        bills = read_bills()
+        self.assertEqual(20, len(bills))
+        self.assertEqual('Electric Ireland', bills[0][0])
+        self.assertEqual(' credit', bills[19][6])
+        
+if __name__ == '__main__':
+    unittest.main()
